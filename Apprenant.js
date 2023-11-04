411a1eea90ccb2fb7644752c8566e3a7d5b21885
ahlamabdelkader <116796820+ahlamabdelkader@users.noreply.github.com>
2023-11-04 15:35:51 +0000

    Create Apprenant.js

---
+++ Apprenant.js
@@ -0,0 +1,15 @@
+[
+    {
+        "ID" : 1,
+        "Nom" : "ahlam",
+        "Prenom" : "med",
+        "Niveau/Classe" : "lycée",
+        "Adresse" : "22234 paris",
+        "Coordonnées_GPS" : "48°51'12.2″N 2°20'55.7″E correspondent aux coordonnées GPS de la ville de Paris",
+        "Numero de telephone" : "22667788",
+        "Numero de wtsp" : "22667788",
+        "Type_paiement_mobile" : "bankily",
+        "Numero paiement" : "22667788",
+        "ID_Famille" : "1",
+    }
+]
